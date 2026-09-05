e6d71062ffd47895e1c11d28f6c7983f11b002f0
JamesZ1110 <zhoupeiyi1001@gmail.com>
2026-09-05 21:41:47 +1000

    Improve code quality

--- src/main/java/Exia.java
+++ src/main/java/Exia.java
@@ -1,56 +1,76 @@
 import java.util.Scanner;
 
 public class Exia {
+    private static final String LINE = "____________________________________________________________";
+    private static final int MAX_TASKS = 100;
+
     public static void main(String[] args) {
-        String line = "____________________________________________________________";
-        Task[] tasks = new Task[100];
+        Task[] tasks = new Task[MAX_TASKS];
         int taskCount = 0;
 
-        System.out.println(line);
+        showGreeting();
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (scanner.hasNextLine()) {
+                String input = scanner.nextLine();
+
+                if (input.equals("bye")) {
+                    showGoodbye();
+                    break;
+                }
+
+                if (input.equals("list")) {
+                    showTaskList(tasks, taskCount);
+                    continue;
+                }
+
+                if (input.startsWith("done ")) {
+                    markTaskAsDone(tasks, input);
+                    continue;
+                }
+
+                tasks[taskCount] = new Task(input);
+                taskCount++;
+                showAddedTask(input);
+            }
+        }
+    }
+
+    public static void showGreeting() {
+        System.out.println(LINE);
         System.out.println("Hello! I'm Exia");
         System.out.println("What can I do for you?");
-        System.out.println(line);
+        System.out.println(LINE);
+    }
 
-        Scanner scanner = new Scanner(System.in);
+    public static void showGoodbye() {
+        System.out.println(LINE);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(LINE);
+    }
 
-        while (scanner.hasNextLine()) {
-            String input = scanner.nextLine();
-
-            if (input.equals("bye")) {
-                System.out.println(line);
-                System.out.println("Bye. Hope to see you again soon!");
-                System.out.println(line);
-                break;
-            }
-
-            if (input.equals("list")) {
-                System.out.println(line);
-                for (int i = 0; i < taskCount; i++) {
-                    System.out.println((i + 1) + "." + tasks[i]);
-                }
-                System.out.println(line);
-                continue;
-            }
-
-            if (input.startsWith("done ")) {
-                int taskNumber = Integer.parseInt(input.substring(5));
-                tasks[taskNumber - 1].markAsDone();
-
-                System.out.println(line);
-                System.out.println("Nice! I've marked this task as done:");
-                System.out.println(tasks[taskNumber - 1]);
-                System.out.println(line);
-                continue;
-            }
-
-            tasks[taskCount] = new Task(input);
-            taskCount++;
-
-            System.out.println(line);
-            System.out.println("added: " + input);
-            System.out.println(line);
+    public static void showTaskList(Task[] tasks, int taskCount) {
+        System.out.println(LINE);
+        for (int i = 0; i < taskCount; i++) {
+            System.out.println((i + 1) + "." + tasks[i]);
         }
+        System.out.println(LINE);
+    }
 
-        scanner.close();
+    public static void markTaskAsDone(Task[] tasks, String input) {
+        int taskNumber = Integer.parseInt(input.substring(5));
+        Task task = tasks[taskNumber - 1];
+        task.markAsDone();
+
+        System.out.println(LINE);
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println(task);
+        System.out.println(LINE);
+    }
+
+    public static void showAddedTask(String input) {
+        System.out.println(LINE);
+        System.out.println("added: " + input);
+        System.out.println(LINE);
     }
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,21 +1,22 @@
 public class Task {
-    protected String description;
-    protected boolean isDone;
+    private final String description;
+    private boolean isDone;
 
     public Task(String description) {
         this.description = description;
-        this.isDone = false;
-    }
-
-    public String getStatusIcon() {
-        return this.isDone ? "X" : " ";
+        isDone = false;
     }
 
     public void markAsDone() {
-        this.isDone = true;
+        isDone = true;
     }
 
+    public String getStatusIcon() {
+        return isDone ? "X" : " ";
+    }
+
+    @Override
     public String toString() {
-        return "[" + this.getStatusIcon() + "] " + this.description;
+        return "[" + getStatusIcon() + "] " + description;
     }
 }
